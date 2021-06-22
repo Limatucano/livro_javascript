937a4b254ae10d89ca7dc1fb54ac66ddeaaa63f6
Matheus Lima <matheusmt31@gmail.com>
2021-06-22 18:00:05 -0300

    operadores

---
+++ cap01/operadores.js
@@ -0,0 +1,26 @@
+let n = 4
+let m = 2
+
+console.log(n - m)
+console.log(n + m)
+console.log(n / m)
+console.log(n % m)
+console.log(n * m)
+
+n /= 2
+m %= 2
+
+console.log("n:" + n)
+console.log("m:" + m)
+
+
+console.log(!!m)
+console.log(!!n)
+
+
+console.log('5 & 5:'+ (5&5)) 
+console.log('5 | 5:'+ (5|5)) 
+console.log('~ 5:'+ (~5)) 
+console.log('1 ^ 5:'+ (1 ^ 5)) 
+console.log('1 << 5:'+ (1 << 5)) 
+console.log('1 >> 5:'+ (1 >> 5)) 
